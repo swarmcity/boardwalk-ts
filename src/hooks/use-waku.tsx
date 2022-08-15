b5115d4c8fd87c4c7d1b7090560263a364f46405
Philippe Schommers <philippe@schommers.be>
2022-08-15 23:31:11 +0200

    fix: waku peers

--- src/hooks/use-waku.tsx
+++ src/hooks/use-waku.tsx
@@ -13,14 +13,9 @@ import type { CreateOptions } from 'js-waku/build/main/lib/waku'
 // Config
 const DEFAULT_SETTINGS = {
 	bootstrap: {
-		wantedNodeCapabilityCount: {
-			relay: 1,
-			store: 1,
-			filter: 1,
-			lightPush: 1,
-		},
-		enrUrl:
-			'enr:-KO4QPkJGPjc3HSWORbSSGwrRPYCQw6KT3hoTtq9wRETQ0iESuRaEwq9gr5bMn53X07_SyWufwZ5jGE6yXf7Y9sArXQBgmlkgnY0gmlwhLyl4beKbXVsdGlhZGRyc4wACgS8peG3Bh9A3QOJc2VjcDI1NmsxoQIZ9LAuNEcxirgJRqJj0FedIiUS7AA1A0URms-svcgYJIN0Y3CC6mCFd2FrdTIP',
+		peers: [
+			'/dns4/ws.waku.apyos.dev/tcp/443/wss/p2p/16Uiu2HAkwAzaMhKTikSVD1djSieWNoJ9jq63kmJG21mmizT8jQDq',
+		],
 	},
 }
 
