49df31dda105457a167957554c5ca186589a9472
Philippe Schommers <philippe@schommers.be>
2022-08-18 14:32:59 +0200

    feat: fetch items from chain and waku and display

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -29,3 +29,9 @@ export const numberToBigInt = (number: number | string, decimals: number) => {
 	)
 	return BigInt(whole + fractionTrimmed + zeroes.join(''))
 }
+
+export const bufferToHex = (buffer: ArrayBuffer) => {
+	return [...new Uint8Array(buffer)]
+		.map((x) => x.toString(16).padStart(2, '0'))
+		.join('')
+}
