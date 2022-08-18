9c343aead32e536a7aa94addef3cfde4ee09e8e0
Philippe Schommers <philippe@schommers.be>
2022-08-18 12:02:38 +0200

    chore: simplify vite config
    The previous configuration is no longer necessary with the new `js-waku` release

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import svgLoader from 'vite-plugin-react-svg'
 import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,25 +12,9 @@ export default defineConfig({
 	},
 	build: {
 		target: ['es2020'],
-		ssr: false,
-	},
-	resolve: {
-		conditions: ['browser', 'module'],
-		alias: {
-			os: 'rollup-plugin-node-polyfills/polyfills/os',
-			'nat-api': 'rollup-plugin-node-polyfills/polyfills/empty',
-		},
 	},
 	optimizeDeps: {
 		esbuildOptions: {
-			define: {
-				global: 'globalThis',
-			},
-			plugins: [
-				NodeGlobalsPolyfillPlugin({
-					buffer: true,
-				}),
-			],
 			supported: {
 				bigint: true,
 			},
