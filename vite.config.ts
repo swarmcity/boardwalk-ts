9c2b8cbc2f697c7221c85411e3d6e0a6ff32eacf
Philippe Schommers <philippe@schommers.be>
2022-09-12 18:13:38 +0200

    chore: remove `vite-plugin-react-svg`

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
-import svgLoader from 'vite-plugin-react-svg'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgLoader()],
+	plugins: [react()],
 	css: {
 		modules: {
 			localsConvention: 'camelCaseOnly',
