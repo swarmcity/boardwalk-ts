a2475aec4533699cd15013405ef588c6b70276f7
Vojtech Simetka <vojtech@simetka.cz>
2022-10-17 10:33:46 +0200

    feat: fixed styling on the login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,30 +1,71 @@
-import { Link } from 'react-router-dom'
-import { ButtonClose } from '../components/button-close'
-import { ACCOUNT_RESTORE, CREATE_ACCOUNT, HOME } from '../routes'
+import { useNavigate } from 'react-router-dom'
+import { Button, IconButton } from '@swarm-city/ui-library'
+import { ACCOUNT_RESTORE, CREATE_ACCOUNT } from '../routes'
+import { getColor } from '../ui/colors'
+import { Typography } from '../ui/typography'
+import { Container } from '../ui/container'
 
-export const Login = () => (
-	<div className="bg-info create-account">
-		<div className="close">
-			<ButtonClose to={HOME} variant="light" />
-		</div>
-		<div className="container">
-			<main className="flex-space">
-				<header>
-					<h1>Let's create an account.</h1>
-					<p>
+export const Login = () => {
+	const navigate = useNavigate()
+	return (
+		<div
+			style={{
+				backgroundColor: getColor('blue'),
+				minHeight: '100vh',
+				width: '100vw',
+				overflowX: 'hidden',
+			}}
+		>
+			<Container>
+				<div style={{ position: 'relative', width: '100%' }}>
+					<div style={{ position: 'absolute', right: 15, top: 15 }}>
+						<IconButton variant="cancelCreation" onClick={() => navigate(-1)} />
+					</div>
+				</div>
+				<div
+					style={{
+						marginTop: 130,
+						marginLeft: 10,
+						marginRight: 10,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						textAlign: 'center',
+					}}
+				>
+					<Typography variant="header-35" color="white">
+						Let's create an account.
+					</Typography>
+					<Typography
+						variant="small-light-12"
+						color="white"
+						style={{ marginTop: 18 }}
+					>
 						No account was found on this device. When you restore or create a
 						new account, it is stored locally on your device.
-					</p>
-				</header>
-				<div className="btns">
-					<Link className="btn btn-info" to={CREATE_ACCOUNT}>
-						create account
-					</Link>
-					<Link className="btn btn-info" to={ACCOUNT_RESTORE}>
-						restore account
-					</Link>
+					</Typography>
+					<div style={{ marginTop: 158 }}>
+						<Button
+							color="blue-light"
+							bg
+							size="large"
+							onClick={() => navigate(CREATE_ACCOUNT)}
+							style={{ margin: 5 }}
+						>
+							create account
+						</Button>
+						<Button
+							color="blue-light"
+							bg
+							size="large"
+							onClick={() => navigate(ACCOUNT_RESTORE)}
+							style={{ margin: 5 }}
+						>
+							restore account
+						</Button>
+					</div>
 				</div>
-			</main>
+			</Container>
 		</div>
-	</div>
-)
+	)
+}
