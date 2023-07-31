ab53318ce3d9292b64d8e1693499ad6b121c45d7
Philippe Schommers <philippe@schommers.be>
2023-07-31 21:12:04 +0200

    chore: use Gateway RPC

--- src/config/production.ts
+++ src/config/production.ts
@@ -9,8 +9,8 @@ export const MARKETPLACE_LIST = '0x72FdB3f1B2A70F4B969864D0B7EcB246B4Ba5F7F'
 export const HASHTAG_FACTORY = '0x1872524b7D5F0BbB975E2E9EE1a226b72c2dB0D3'
 export const APP_TOKEN = constants.AddressZero
 
-const { http } = WAGMI_CHAIN.rpcUrls.default
-const webSocket = ['wss://rpc.gnosischain.com/wss']
+const http = ['https://rpc.gnosis.gateway.fm/']
+const webSocket = ['wss://rpc.gnosis.gateway.fm/ws']
 
 WAGMI_CHAIN.rpcUrls = {
 	public: { http, webSocket },
